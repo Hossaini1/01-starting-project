919694210d07ab8a6792ef66c207bd617e2d1417
hossaini <1989hossaini1@gmail.com>
2024-10-31 10:04:10 +0100

    added new exercise extrating parameter dynamic router angular

--- src/app/users/user-tasks/user-tasks.component.ts
+++ src/app/users/user-tasks/user-tasks.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject, Input, input } from '@angular/core';
+import { UsersService } from '../users.service';
 
 @Component({
   selector: 'app-user-tasks',
@@ -6,4 +7,29 @@ import { Component } from '@angular/core';
   templateUrl: './user-tasks.component.html',
   styleUrl: './user-tasks.component.css',
 })
-export class UserTasksComponent {}
+export class UserTasksComponent {
+
+  userId=input.required<string>();
+
+  private userService = inject(UsersService);
+
+  userName = computed(()=>this.userService.users.find((user)=>user.id===this.userId())?.name);  
+
+
+  //alternative mit @Input dekorator 
+  // private userService = inject(UsersService);
+  // userName: string | undefined;
+  // private _userId!: string;
+
+  // @Input({ required: true })
+  // set userId(uid: string) {
+  //   this._userId = uid;
+  //   this.userName = this.userService.users.find((u) => u.id === uid)?.name
+  // }
+
+  // get userId(): string {
+  //   return this._userId
+  // }
+
+
+}
