acea2c973c47a08a553d68ff9e573e299f6b59be
Hossaini <1989hossaini1@gmail.com>
2024-11-13 10:00:54 +0100

    query parameter daten manipulation eingefügt die uebung

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,4 +1,4 @@
-import { Component,inject,input,computed, DestroyRef, OnInit } from '@angular/core';
+import { Component, inject, input, computed, DestroyRef, OnInit, signal } from '@angular/core';
 import { ActivatedRoute, RouterLink } from '@angular/router';
 
 
@@ -10,23 +10,31 @@ import { TasksService } from './tasks.service';
   standalone: true,
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css',
-  imports: [TaskComponent,RouterLink],
+  imports: [TaskComponent, RouterLink],
 })
-export class TasksComponent implements OnInit{
+export class TasksComponent implements OnInit {
   userId = input.required<string>();
+  // alternative für die Order mit input anstatt signal
   // order = input<'asc'|'desc'>();
+  order = signal<'asc' | 'desc'>('desc')
 
-  private tasksService =inject(TasksService);
-  userTasks= computed(()=> this.tasksService.allTasks().filter((task)=>task.userId===this.userId()));
+  private tasksService = inject(TasksService);
+  userTasks = computed(() => this.tasksService.allTasks().filter((task) => task.userId === this.userId()).sort((a, b) => {
+    if (this.order() === 'desc') {
+      return a.id > b.id ? -1 : 1;
+    } else {
+      return a.id > b.id ? 1 : -1;
+    }
+  }));
 
-  order?:'asc'|'desc';
+
   private activatedRoute = inject(ActivatedRoute);
   private destroyRef = inject(DestroyRef);
 
-ngOnInit(): void {
-  const subscribtion=this.activatedRoute.queryParams.subscribe({
-    next:(params)=>(this.order=params['order']),
-  });
-this.destroyRef.onDestroy(()=>subscribtion.unsubscribe())
-} 
+  ngOnInit(): void {
+    const subscribtion = this.activatedRoute.queryParams.subscribe({
+      next: (params) => (this.order.set(params['order'])),
+    });
+    this.destroyRef.onDestroy(() => subscribtion.unsubscribe())
+  }
 }
